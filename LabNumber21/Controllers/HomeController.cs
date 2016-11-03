c97cf2ebbe9918bff2cf9be9f9cd48f8ba3a42eb
athikumar72 <athikumar72@gmail.com>
2016-11-03 16:53:07 -0400

    Lab21 Final commit
    final commit

---
+++ LabNumber21/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LabNumber21.Models;
+
+namespace LabNumber21.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Homer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpGet]
+
+        public ActionResult Registration()
+        {
+            
+            
+                return View();
+
+            }
+            
+
+           [HttpPost]
+        public ViewResult Registration(RegistrationForm registrationForm)
+
+        {
+            //TODO:Email Response to the party organizer
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", registrationForm);
+            }
+            else
+            {
+                return View();
+
+            }
+            
+        }
+
+    }
+}
---
+++ LabNumber21/Models/RegistrationForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace LabNumber21.Models
+{
+    public class RegistrationForm
+    {
+        //[Required(ErrorMessage = "Please enter your name")]
+        [Required(ErrorMessage = "Please enter your  Firstname")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Second name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter Gender")]
+        public string Gender { get; set; }
+        [Required(ErrorMessage = "Please enter your Age")]
+        public string Age { get; set; }
+        [Required(ErrorMessage = "Please enter your Type of Study")]
+        public string TypeOfStudy { get; set; }
+
+
+
+      
+    }
+}
